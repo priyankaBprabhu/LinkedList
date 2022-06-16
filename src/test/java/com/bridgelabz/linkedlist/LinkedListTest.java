444ae55949e52cec70fd4c206e0d7f7909a155c2
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-06-17 00:17:38 +0530

    Added test case to search and insert an element  in  linked list

--- src/test/java/com/bridgelabz/linkedlist/LinkedListTest.java
+++ src/test/java/com/bridgelabz/linkedlist/LinkedListTest.java
@@ -4,14 +4,23 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 public class LinkedListTest {
+    IList<Integer> linkedList = new LinkedList<>() ;
     @Test
     public void SearchNodeWithValue_WhenFind_ShouldReturnTrue() {
-
-        IList<Integer> linkedList = new LinkedList<>() ;
         linkedList.add(56);
         linkedList.add(30);
         linkedList.add(70);
         Assert.assertTrue(linkedList.search(30));
+    }
+    @Test
+    public void SearchNodeWithValue_WhenFind_InsertNewNode() {
+        linkedList.add(56);
+        linkedList.add(30);
+        linkedList.add(70);
+        linkedList.insert((linkedList.index(30) + 1),40);
+        System.out.println(linkedList);
+        Assert.assertTrue(linkedList.search(40));
+
     }
 }
 
