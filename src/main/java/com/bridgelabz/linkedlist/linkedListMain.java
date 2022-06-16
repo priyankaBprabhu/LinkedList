5e291c12de1a544cc8c19a366b7bdeacffbda509
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-06-16 22:59:26 +0530

    Added Linked list implementation message on master branch

---
+++ src/main/java/com/bridgelabz/linkedlist/linkedListMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.linkedlist;
+
+public class linkedListMain {
+    public static void main(String[] args) {
+        System.out.println("Linked list Implementations");
+    }
+}
