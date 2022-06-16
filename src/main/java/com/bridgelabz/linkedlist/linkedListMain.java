cfc21570c4d7eb4d6a3cd986bcc551f34757a581
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-06-16 23:29:43 +0530

    Added code to insert an element in  linked list

--- src/main/java/com/bridgelabz/linkedlist/linkedListMain.java
+++ src/main/java/com/bridgelabz/linkedlist/linkedListMain.java
@@ -4,9 +4,9 @@ public class linkedListMain {
     public static void main(String[] args) {
         System.out.println("Linked list Implementations");
         IList<Integer> linkedList = new LinkedList<>() ;
-        linkedList.append(56);
-        linkedList.append(30);
-        linkedList.append(70);
+        linkedList.add(56);
+        linkedList.add(70);
+        linkedList.insert(1, 30);
         System.out.println(linkedList);
     }
 }
