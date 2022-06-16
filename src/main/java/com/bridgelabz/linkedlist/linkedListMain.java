1016ef2775aa530c27048d785f3686ffaac1b6b1
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-06-16 23:23:14 +0530

    Added code to add a elements using append method in linked list

--- src/main/java/com/bridgelabz/linkedlist/linkedListMain.java
+++ src/main/java/com/bridgelabz/linkedlist/linkedListMain.java
@@ -4,9 +4,9 @@ public class linkedListMain {
     public static void main(String[] args) {
         System.out.println("Linked list Implementations");
         IList<Integer> linkedList = new LinkedList<>() ;
-        linkedList.addFirst(70);
-        linkedList.addFirst(30);
-        linkedList.addFirst(56);
+        linkedList.append(56);
+        linkedList.append(30);
+        linkedList.append(70);
         System.out.println(linkedList);
     }
 }
